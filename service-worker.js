1f0aa4bab1a756034553c63058f59c83dcdf0ea8
Team LevX <team@levx.app>
2020-11-05 06:18:28 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8063fc9afd167538df692cf31ebb7052.js"
+  "./precache-manifest.69dde4afa17813001e419c50df4e4560.js"
 );
 
 workbox.skipWaiting();
